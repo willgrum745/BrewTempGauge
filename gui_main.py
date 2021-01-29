84d96a40489474095d0043bb893a832c68588b2f
Will <78219111+willgrum745@users.noreply.github.com>
2021-01-29 14:59:15 -0500

    Update gui_main.py

--- gui_main.py
+++ gui_main.py
@@ -9,16 +9,19 @@ relay_limit3 = 70
 temp1 = 75
 temp2 = 60
 temp3 = 88
+    #others
+fullscreen = False
 #create window and configure it
 root = tk.Tk()
+root.title("Temp Controller rev1")
 root.geometry('480x320')
 root.rowconfigure([1,2,3], minsize=50, weight=1)
 root.columnconfigure([0, 1, 2, 3, 4], minsize=50, weight=1)
 
 #function
-def generic_label(txt, col):
-    labelSet = tk.Label(root, text=txt)
-    labelSet.grid(row=0, column=col)
+def generic_label(txt, col, row_):
+    labelSet = tk.Label(root, text=txt, font=("bold", 15))
+    labelSet.grid(row=row_, column=col)
     #increase and decrease temp set values per relay/thermistor
 def increase_r1():
     global relay_limit1
@@ -65,6 +68,15 @@ def update_light():
         light3.itemconfig(indicatorLight3, fill='red')
     root.after(500, update_light)
 
+def fullscreen_toggle():
+    global fullscreen
+    if fullscreen == False:
+        root.attributes("-fullscreen", True)
+        fullscreen = True
+    else:
+        root.attributes("-fullscreen", False)
+        fullscreen = False
+
 
 
 #set up gui items
@@ -94,28 +106,36 @@ indicatorLight3 = light3.create_oval(15,15,35,35, fill="green")
 #setTemp = tk.Label(root, text=relay_limit1)
 #place gui items
     #headers
-generic_label("Temp Limit", 1)
-generic_label("Current Temp", 3)
+generic_label("Temp Limit", 1, 0)
+generic_label("Current Temp", 3, 0)
     #temp and gauge 1
-but_dec1.grid(row=1,column=0, sticky="nsew")
-setTemp1.grid(row=1, column=1, sticky="nsew")
-but_inc1.grid(row=1,column=2, sticky="nsew")
-currTemp1.grid(row=1,column=3, sticky="nsew")
-light1.grid(row=1, column=4, stick="nsew")
+generic_label("Kettle 1", 0, 1)
+but_dec1.grid(row=1,column=1, sticky="nsew")
+setTemp1.grid(row=1, column=2, sticky="nsew")
+but_inc1.grid(row=1,column=3, sticky="nsew")
+currTemp1.grid(row=1,column=4, sticky="nsew")
+light1.grid(row=1, column=5, sticky="nsew")
     #temp and gauge 2
-but_dec2.grid(row=2,column=0, sticky="nsew")
-setTemp2.grid(row=2, column=1, sticky="nsew")
-but_inc2.grid(row=2,column=2, sticky="nsew")
-currTemp2.grid(row=2,column=3, sticky="nsew")
-light2.grid(row=2, column=4, stick="nsew")
-    #temp and gauge 2
-but_dec3.grid(row=3,column=0, sticky="nsew")
-setTemp3.grid(row=3, column=1, sticky="nsew")
-but_inc3.grid(row=3,column=2, sticky="nsew")
-currTemp3.grid(row=3,column=3, sticky="nsew")
-light3.grid(row=3, column=4, stick="nsew")
+generic_label("Kettle 2", 0, 2)
+but_dec2.grid(row=2,column=1, sticky="nsew")
+setTemp2.grid(row=2, column=2, sticky="nsew")
+but_inc2.grid(row=2,column=3, sticky="nsew")
+currTemp2.grid(row=2,column=4, sticky="nsew")
+light2.grid(row=2, column=5, sticky="nsew")
+    #temp and gauge 3
+generic_label("Kettle 1", 0, 3)
+but_dec3.grid(row=3,column=1, sticky="nsew")
+setTemp3.grid(row=3, column=2, sticky="nsew")
+but_inc3.grid(row=3,column=3, sticky="nsew")
+currTemp3.grid(row=3,column=4, sticky="nsew")
+light3.grid(row=3, column=5, sticky="nsew")
+    #full screen toggle button
+toggleFS = tk.Button(root, text="Toggle Fullscreen", command=fullscreen_toggle)
+toggleFS.grid(row=4,column=0, sticky="nsew")
 #other stuff in GUI
 update_light()
 
-root.mainloop()
 
+
+#loop
+root.mainloop()
